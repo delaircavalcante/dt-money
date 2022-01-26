c5eb63a44565a939c93d0e78c81111e7def721d5
delaircavalcante <delairc6@gmail.com>
2022-01-26 15:20:57 -0300

    .\yarn.lockDt-money inserida

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+// import styled from 'styled-components'
+
+import { useState } from "react";
+import { Dashbord } from "./components/Dashbord";
+import { Header } from "./components/Header";
+import { GlobalStyle } from "./styles/global";
+import     Modal from "react-modal"
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsProvider } from "./hooks/useTransactions";
+
+
+
+
+Modal.setAppElement("#root")
+
+export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen]=useState(false);
+
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+  
+  return (
+      <TransactionsProvider >
+        <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+        <Dashbord/>
+        <NewTransactionModal
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        />
+        <GlobalStyle/>
+      </TransactionsProvider>
+      
+)
+}
---
+++ src/components/Dashbord/index.tsx
@@ -0,0 +1,14 @@
+import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
+import { Container } from "./styles";
+
+
+export function Dashbord(){
+    return(
+        <Container>
+            <Summary/>
+            <TransactionsTable/>
+        </Container>
+    );
+
+}
